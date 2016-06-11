d5da030d9f597ac4569a6446c3fe0da27d882f04
gabriel <furstenheim@gmail.com>
2016-06-11 16:54:08 +0200

    Add User model

---
+++ backend/api/models/User.js
@@ -0,0 +1,32 @@
+module.exports = {
+   attributes : {
+      username : {
+         type : 'string',
+         required : true
+      },
+      passHash : {
+         type : 'string',
+         required : true
+      },
+      name : {
+         type : 'string',
+         required : true
+      },
+      lastName : {
+         type : 'string',
+         required : true
+      },
+      company : {
+         type : 'string',
+         required : true
+      },
+      bank_id : {
+         type : 'string',
+         required : true
+      },
+      account_id : {
+         type : 'string',
+         required : true
+      },
+   }
+}
