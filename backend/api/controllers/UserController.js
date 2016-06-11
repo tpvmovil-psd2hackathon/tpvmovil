f2adb739439ca8096c8f2f3005061213f7369b0b
Alfredo Arellano <alfredo.arellano@hpe.com>
2016-06-11 16:26:16 +0200

    get user token from api

--- backend/api/controllers/UserController.js
+++ backend/api/controllers/UserController.js
@@ -1,8 +1,13 @@
-module.exports = {   
-   
-   login: function (req, res) {
-      req.badRequest
-   }
-
-
+module.exports = {
+   loginClient
 };
+
+
+function loginClient(req, res) {
+   console.log("LOGIN");
+   LoginService.getUserToken(req.body.user, req.body.pass).then(function (data) {
+      res.send(data)
+   }).catch(function (e) {
+      res.badRequest("Bad username/password");
+   })
+}
