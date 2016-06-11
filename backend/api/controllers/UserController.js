48c6d90d36a129394a96fa210dbdcf3c08baeb65
gabriel <furstenheim@gmail.com>
2016-06-11 18:40:19 +0200

    Fill user

--- backend/api/controllers/UserController.js
+++ backend/api/controllers/UserController.js
@@ -7,7 +7,7 @@ function login(req, res){
    q.spawn(function*(){
       try {
          var user = yield UserService.findOne(req.body.username, req.body.pass)
-         res.ok()
+         res.send(user)
       } catch (e){
          console.error(e)
          res.serverError()
