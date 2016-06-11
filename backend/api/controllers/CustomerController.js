99a9d40cc2b3359d417e0ab47a6d8871cb7d3e7b
gabriel <furstenheim@gmail.com>
2016-06-11 17:14:02 +0200

    Rename user to customer

--- backend/api/controllers/CustomerController.js
+++ backend/api/controllers/CustomerController.js
@@ -5,7 +5,7 @@ module.exports = {
 
 function loginCustomer(req, res) {
    console.log("LOGIN");
-   LoginService.getUserToken(req.body.user, req.body.pass).then(function (data) {
+   CustomerService.getCustomerToken(req.body.customerName, req.body.customerPass).then(function (data) {
       res.send(data)
    }).catch(function (e) {
       res.badRequest("Bad username/password");
